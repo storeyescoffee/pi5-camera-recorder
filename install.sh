2c4fac8cfdb75431f730a1f6a863de10663244d8
mohcine sahtani <mohcine.sahtani@gmail.com>
2026-03-19 06:30:53 +0000

    finished ./install.sh script

--- install.sh
+++ install.sh
@@ -4,4 +4,13 @@ sudo apt install -y \
   python3-av \
   python3-picamera2 \
   python3-boto3 \
-  ffmpeg
+  ffmpeg \
+  at
+
+
+# Ensure @reboot cron entry exists (no duplicates)
+CRON_LINE='@reboot cd $HOME/pi5-camera-recorder && python3 main.py'
+(crontab -l 2>/dev/null | grep -Fv "$CRON_LINE"; echo "$CRON_LINE") | crontab -
+
+# Start once after 1 minute using at
+echo "cd $HOME/pi5-camera-recorder && python3 main.py" | at now + 1 minute
